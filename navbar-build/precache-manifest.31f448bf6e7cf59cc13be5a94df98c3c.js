93d78f1029ae0939b0b762aef4248dff18f34323
amy-qian8 <67285038+amy-qian8@users.noreply.github.com>
2020-06-30 13:20:32 -0700

    Add files via upload

---
+++ navbar-build/precache-manifest.31f448bf6e7cf59cc13be5a94df98c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0198b1d28f3398b996d32f4fd137ea5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e2b3820298e10bf17b0",
+    "url": "/static/css/main.ca2462d9.chunk.css"
+  },
+  {
+    "revision": "a01f15601da3e9d440d7",
+    "url": "/static/js/2.2ea9cfb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ea9cfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2b3820298e10bf17b0",
+    "url": "/static/js/main.809fc33f.chunk.js"
+  },
+  {
+    "revision": "2048b6c46f34851a1713",
+    "url": "/static/js/runtime-main.d4a21cb6.js"
+  }
+]);
